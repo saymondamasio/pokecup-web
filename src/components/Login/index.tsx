0e0c30db7e72da6d0c8ab8e8b626fa6621dc407e
Daniel <daniel.carvalho@agenciaeplus.com.br>
2022-11-25 02:44:13 -0300

    style login page

--- src/components/Login/index.tsx
+++ src/components/Login/index.tsx
@@ -4,17 +4,14 @@ import * as S from "./styles";
 export const Login = () => {
   const { data: session } = useSession();
 
-  if (session) {
-    return (
-      <S.Wrapper>
-        Signed in as {session?.user?.email} <br />
-        <button onClick={() => signOut()}>Sair</button>
-      </S.Wrapper>
-    );
-  } else
-    return (
-      <S.Wrapper>
-        <button onClick={() => signIn()}>Entrar</button>
-      </S.Wrapper>
-    );
+  return session ? (
+    <S.Wrapper>
+      Signed in as {session?.user?.email} <br />
+      <button onClick={() => signOut()}>Sair</button>
+    </S.Wrapper>
+  ) : (
+    <S.Wrapper>
+      <S.LoginButton onClick={() => signIn()}>Fazer login </S.LoginButton>
+    </S.Wrapper>
+  );
 };
